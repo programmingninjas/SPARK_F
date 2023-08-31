b2de04d724ded1a3b1fb4dd680de1c11cce3b931
AlphaCupcake10 <alphacupcake@outlook.com>
2023-09-01 01:23:02 +0530

    Landing Page Content

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -30,6 +30,65 @@ function App() {
           </div>
           <div className="bg-secondary w-full">b</div>
         </section>
+        <section className="flex mt-48 flex-wrap flex-row-reverse md:flex-nowrap">
+          <div className="w-full p-8 lg:p-16 flex flex-col gap-48">
+            <div>
+              <h1 className="text-3xl md:text-5xl font-medium">Why <span className="gradient-text">Spark?</span></h1>
+              <p className="mt-8 text-md md:text-xl">
+                Paragraph explaining the problem statement.
+                <br />
+                Elit. Nostrum qui nihil inventore atque reprehenderit corrupti.
+              </p>
+            </div>
+            <div>
+              <h1 className="text-3xl md:text-5xl font-medium">Our <span className="gradient-text">Objective.</span></h1>
+              <p className="mt-8 text-md md:text-xl">
+              Our Approach to solve the problem.
+                <br />
+                Elit. Nostrum qui nihil inventore atque reprehenderit corrupti.
+              </p>
+            </div>
+            <div>
+              <h1 className="text-3xl md:text-5xl font-medium">Our <span className="gradient-text">Solution.</span></h1>
+              <p className="mt-8 text-md md:text-xl">
+                Solution to the problem.
+                <br />
+                Elit. Nostrum qui nihil inventore atque reprehenderit corrupti.
+              </p>
+            </div>
+          </div>
+          <div className="w-full">
+            <div className="bg-accent h-96 sticky top-1/4">b</div>
+          </div>
+        </section>
+        <section className="py-48">
+          <div className="flex flex-wrap gap-4 p-4">
+            <div className="bg-accent text-light grow p-16 rounded-lg flex flex-col items-center justify-center">
+              <h1 className="text-4xl md:text-7xl font-bold">1 in 1000</h1>
+              <h1>Children are diagnosed with down syndrome</h1>
+            </div>
+            <div className="bg-primary text-light grow p-16 rounded-lg flex flex-col items-center justify-center">
+              <h1 className="text-4xl md:text-7xl font-bold">1 in 1000</h1>
+              <p>Children are diagnosed with down syndrome</p>
+            </div>
+            <div className="bg-secondary grow p-16 rounded-lg flex flex-col items-center justify-cente">
+              <h1 className="text-4xl md:text-7xl font-bold">1 in 1000</h1>
+              <p>Children are diagnosed with down syndrome</p>
+            </div>
+            <div className="bg-accent text-light grow-[5] p-16 rounded-lg flex flex-col items-center justify-center">
+              <h1 className="text-4xl md:text-7xl font-bold">1 in 1000</h1>
+              <p>Children are diagnosed with down syndrome</p>
+            </div>
+            <div className="bg-primary text-light grow-[10] p-16 rounded-lg flex flex-col items-center justify-center">
+              <h1 className="text-4xl md:text-7xl font-bold">1 in 1000</h1>
+              <p>Children are diagnosed with down syndrome</p>
+            </div>
+            <div className="bg-secondary grow p-16 rounded-lg flex flex-col items-center justify-cente">
+              <h1 className="text-4xl md:text-7xl font-bold">1 in 1000</h1>
+              <p>Children are diagnosed with down syndrome</p>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
     </>
