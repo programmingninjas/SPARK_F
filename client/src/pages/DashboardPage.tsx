63913781adf0e75b9585e66d997e499118f218fe
AlphaCupcake10 <alphacupcake@outlook.com>
2024-02-20 13:53:22 +0530

    Rewrote dashboard requests

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -62,38 +62,24 @@ function DashboardPage() {
   const auth = useAuth();
   const [dob, setDob] = useState("");
   const modal = useModal();
-  async function getDob() {
-    let response = await auth?.APIFunctions.GetRequest("/user/me", true);
-    if (response.status == 200) {
-      setDob(response.data.dob);
-    }
-  }
-
-  useEffect(() => {
-    getDob();
-  }, [])
 
   const gap = "gap-4";
 
-  const [evaluationStatus,setEvaluationStatus] = useState(false);
-
   const [ageResults, setAgeResult] = useState([]);
-  //TODO Change any to the correct type
-  const [isaaResult, setIsaaResult] = useState<any>({});
+  const [evaluationStatus, setEvaluationStatus] = useState(false);
   const [physicalResult, setPhysicalResult] = useState(0);
-  const [range, setRange] = useState("");
+  const [developmentalAge, setDevelopmentalAge] = useState(0);
   const [months, setMonths] = useState(0);
   const [user, setUser] = useState("");
 
-  const [developmentalAge, setDevelopmentalAge] = useState(0);
   const [category, setCategory] = useState("");
-  const [autism, setAutism] = useState("");
+  const [range, setRange] = useState("");
 
   async function getUser() {
     let response = await auth?.APIFunctions.GetRequest("/user/me", true);
     if (response.status == 200) {
-      setDob(response.data.dob);
-      setUser(response.data._id);
+      setDob(response?.data?.dob);
+      setUser(response?.data?._id);
     }
   }
 
@@ -101,36 +87,109 @@ function DashboardPage() {
     getUser();
   }, [])
 
-  function calculateMonths(dob: string){
-    const dateRegex: RegExp = /^(\d{4})-(\d{2})-(\d{2})$/;
-    const match: RegExpMatchArray | null = dob.match(dateRegex);
-
-    if (!match) {
-      throw new Error('Invalid date format. Please provide date in the format YYYY-MM-DD.');
-    }
-
-    const [, yearStr, monthStr, dayStr]: string[] = match;
-    const day: number = parseInt(dayStr, 10);
-    const month: number = parseInt(monthStr, 10);
-    const year: number = parseInt(yearStr, 10);
-
-    const today: Date = new Date();
-    const birthDate: Date = new Date(year, month - 1, day); // month is 0-indexed in JavaScript Dates
-
-    // Calculate the difference in months
-    let months = (today.getFullYear() - birthDate.getFullYear()) * 12 + (today.getMonth() - birthDate.getMonth());
-
-    // If the current day of the month is before the birth day, decrement months by 1
-    if (today.getDate() < birthDate.getDate()) {
-      months--;
-    }
-
-    setMonths(months);
-  }
   useEffect(() => {
     if (!user) return;
     calculateMonths(dob);
+    getphysicalResult();
+    getAgeResult();
   }, [user])
+
+  useEffect(() => {
+    if (!ageResults) return;
+    //check if ageResults is an Array
+    if(!Array.isArray(ageResults)) return;
+    setDevelopmentalAge(evaluateResults(months, ageResults));
+  }, [ageResults])
+
+  useEffect(()=>{
+    if(!months)return;
+    getEvaluationStatus();
+  },[months])
+
+  useEffect(() => {
+    if (!developmentalAge) return;
+    setCategory(getCategory());
+  }, [developmentalAge])
+
+  async function getAgeResult() {
+    let months = calculateMonths(dob);
+    let range = getMonthRange(months);
+    let response = await auth?.APIFunctions.GetRequest("/scale/result/" + range, true, { user });
+    setMonths(months);
+    setRange(range);
+    if (response.status == 200) {
+      setAgeResult(response?.data);
+    }
+  } 
+
+  interface MinAge {
+    [minAge: string]: number;
+  }
+  const minAge: MinAge = {
+    'birth_to_five': 0,
+    'six_to_ten': 6,
+    'eleven_to_fifteen': 11,
+    'sixteen_to_twenty': 16,
+    'twentyone_to_twentyfive': 21,
+    'twentysix_to_thirty': 26,
+    'thirtyone_to_thirtyfive': 31,
+    'thirtysix_to_forty': 36,
+    'fortyone_to_fiftynine': 41,
+    'sixty_to_seventyone': 60
+  };
+
+  function getCategory(): string {
+    if (months === developmentalAge) {
+      return "Actual Age";
+    } else {
+      return "Younger Age";
+    }
+  }
+
+  type Response = "yes" | "no";  
+  function evaluateResults(age: number, results: Response[]): number {
+    const countYes = results.filter(result => result === "yes").length;
+    const countNo = results.filter(result => result === "no").length;
+
+    const totalResponses = results.length;
+    const halfResponses = totalResponses / 2;
+
+    if (countYes === totalResponses - 1 && countNo === 1) {
+      // All but one response is "yes"
+      return age;
+    } else if (countYes >= halfResponses) {
+      // Half or more responses are "yes"
+      return minAge[range] + 2;
+    } else {
+      // More than half responses are "no"
+      return minAge[range];
+    }
+  }
+
+  async function getphysicalResult() {
+    let response = await auth?.APIFunctions.GetRequest("/physicaltime", true, { user });
+    if (response.status == 200) {
+      setPhysicalResult(response?.data?.results.physicalResult);
+    }
+  }
+
+  async function getEvaluationStatus() {
+    if ((months/12) <=6){
+    
+      let range = getMonthRange(months);
+      let response = await auth?.APIFunctions.GetRequest("/scale/result/" + range, true, { user });
+      if (response?.data){
+        setEvaluationStatus(true);
+      }
+    }
+    else{
+      console.log("hihi")
+      let response = await auth?.APIFunctions.GetRequest("/physicaltime", true, { user });
+      if (response?.data){
+        setEvaluationStatus(true);
+      }
+    }
+  }
 
   interface MonthRange {
     [months: number]: string;
@@ -160,48 +219,9 @@ function DashboardPage() {
 
     return monthRange;
   }
-  async function getEvaluationStatus() {
-    if ((months/12) <=6){
-    
-      let range = getMonthRange(months);
-      let response = await auth?.APIFunctions.GetRequest("/scale/result/" + range, true, { user });
-      if (response.data){
-        setEvaluationStatus(true);
-      }
-    }
-    else{
-      console.log("hihi")
-      let response = await auth?.APIFunctions.GetRequest("/physicaltime", true, { user });
-      if (response.data){
-        // setEvaluationStatus(true);
-      }
-    }
-  }
 
-  useEffect(()=>{
-    if(!months)return;
-    getEvaluationStatus();
-  },[months])
 
-  if (evaluationStatus){
-
-  interface MinAge {
-    [minAge: string]: number;
-  }
-  const minAge: MinAge = {
-    'birth_to_five': 0,
-    'six_to_ten': 6,
-    'eleven_to_fifteen': 11,
-    'sixteen_to_twenty': 16,
-    'twentyone_to_twentyfive': 21,
-    'twentysix_to_thirty': 26,
-    'thirtyone_to_thirtyfive': 31,
-    'thirtysix_to_forty': 36,
-    'fortyone_to_fiftynine': 41,
-    'sixty_to_seventyone': 60
-  };
-
-  function calculateMonths(dob: string): number {
+  function calculateMonths(dob: string) {
     const dateRegex: RegExp = /^(\d{4})-(\d{2})-(\d{2})$/;
     const match: RegExpMatchArray | null = dob.match(dateRegex);
 
@@ -225,127 +245,9 @@ function DashboardPage() {
       months--;
     }
 
+    setMonths(months);
     return months;
   }
-  async function getAgeResult() {
-    let months = calculateMonths(dob);
-    let range = getMonthRange(months);
-    let response = await auth?.APIFunctions.GetRequest("/scale/result/" + range, true, { user });
-    setMonths(months);
-    setRange(range);
-    if (response.status == 200) {
-      setAgeResult(response.data);
-    }
-  } 
-  
-  useEffect(() => {
-    if (!user) return;
-    getAgeResult();
-  }, [user])
-
-  type Response = "yes" | "no";
-
-  function evaluateResults(age: number, results: Response[]): number {
-    const countYes = results.filter(result => result === "yes").length;
-    const countNo = results.filter(result => result === "no").length;
-
-    const totalResponses = results.length;
-    const halfResponses = totalResponses / 2;
-
-    if (countYes === totalResponses - 1 && countNo === 1) {
-      // All but one response is "yes"
-      return age;
-    } else if (countYes >= halfResponses) {
-      // Half or more responses are "yes"
-      return minAge[range] + 2;
-    } else {
-      // More than half responses are "no"
-      return minAge[range];
-    }
-  }
-
-  useEffect(() => {
-    if (!ageResults) return;
-    setDevelopmentalAge(evaluateResults(months, ageResults));
-  }, [ageResults])
-
-  function getCategory(): string {
-    if (months === developmentalAge) {
-      return "Actual Age";
-    } else {
-      return "Younger Age";
-    }
-  }
-
-  useEffect(() => {
-    if (!developmentalAge) return;
-    setCategory(getCategory());
-  }, [developmentalAge])
-
-  function calculateAutismCategory() {
-    const categories = Object.keys(isaaResult);
-    let totalScore = 0;
-
-    categories.forEach(category => {
-      const responses = isaaResult[category];
-      console.log(responses)
-      //TODO Change any to the correct type
-      const scoreMap: any = {
-        "rarely": 1,
-        "sometimes": 2,
-        "frequently": 3,
-        "mostly": 4,
-        "always": 5
-      };
-
-      for (let i = 0; i < responses.length; i++) {
-        totalScore += scoreMap[responses[i]]
-      }
-    });
-    // Determine the category based on the total score
-    let category;
-    if (totalScore >= 153) {
-      category = "Severe Autism";
-    } else if (totalScore >= 107 && totalScore < 153) {
-      category = "Moderate Autism";
-    } else if (totalScore >= 70 && totalScore < 106) {
-      category = "Mild Autism";
-    }
-    else {
-      category = "No Autism";
-    }
-
-    return category;
-  }
-
-  async function getisaaResult() {
-    let response = await auth?.APIFunctions.GetRequest("/isaa/result/", true, { user });
-    if (response.status == 200) {
-      setIsaaResult(response.data.results);
-    }
-  }
-  useEffect(() => {
-    if (!user) return;
-    getisaaResult();
-  }, [user])
-
-  useEffect(() => {
-    if (!isaaResult) return;
-    setAutism(calculateAutismCategory());
-  }, [isaaResult])
-
-  async function getphysicalResult() {
-    let response = await auth?.APIFunctions.GetRequest("/physicaltime", true, { user });
-    if (response.status == 200) {
-      setPhysicalResult(response.data.results.physicalResult);
-    }
-  }
-
-  useEffect(() => {
-    if (!user) return;
-    getphysicalResult();
-  }, [user]) 
-}
 
   return (
     <>
@@ -408,7 +310,7 @@ function DashboardPage() {
             </div>
             <div className="grow card p-4 flex justify-between items-center">
               <h1 className="font-semibold text-md">Evaluation Status</h1>
-              <h1 className="font-semibold text-md text-primary">Pending...</h1>
+              <h1 className="font-semibold text-md text-primary">{ evaluationStatus?"Evaluated":"Pending..." }</h1>
             </div>
           </div>
           <div className="card p-4 bg-white h-96 w-full lg:w-160 grow">
@@ -446,51 +348,53 @@ function DashboardPage() {
             </Link>
           </>)}
         </div>
-        {(evaluationStatus==true)?(<>
-          <h1 className="text-2xl sm:text-4xl lg:text-6xl mt-24 font-semibold">
-          <span className="gradient-text">Training</span> Modules.
-        </h1>
-        <h1 className="text-md sm:text-2xl lg:text-3xl opacity-70">
-          Our Recommendations
-        </h1>
-        <div className="flex gap-4 flex-wrap text-light text-xl font-medium mt-8">
-          {
-            category == "Younger" && (months / 12) <= 6 ? (
-            <>          
-            <Link className="card bg-primary w-96 grow hover:grow-[2] grid text-center py-16" to={"/training/3dtext"}>
-              3D Alphabets
-            </Link>
-            </>) : (<>
-            <Link className="card bg-primary w-96 grow hover:grow-[2] grid text-center py-16" to={"/training/colorMatch"}>
-                Color Matching
-            </Link>
-            <Link className="card bg-primary w-96 grow hover:grow-[2] grid text-center py-16" to={"/training/shapeDetection"}>
-            Shape Detection
-           </Link>
-            </>)
-          }
-          {((months / 12) >= 6 && physicalResult<7842?(<>
-            <Link className="card bg-primary w-96 grow hover:grow-[2] grid text-center py-16" to={"/training/colorMatch"}>
-                Color Matching
-            </Link>
-            <Link className="card bg-primary w-96 grow hover:grow-[2] grid text-center py-16" to={"/training/shapeDetection"}>
-            Shape Detection
-           </Link>
-           <Link className="card bg-primary w-96 grow hover:grow-[2] grid text-center py-16" to={"/training/detection"}>
-            Object Recognition
-          </Link>
-          </>):(null))}
-          {((months / 12) >= 6 && physicalResult>=7842)?(<>
-            <Link className="card bg-primary w-96 grow hover:grow-[2] grid text-center py-16" to={"/training/faceExpressionDetection"}>
-            Face Expression Detection
-          </Link>
-          <Link className="card bg-primary w-96 grow hover:grow-[2] grid text-center py-16" to={"/training/memoryGame"}>
-            Memory Game
-          </Link>
-          </>):(null)}
-        </div>
-        </>):null}
-        
+        {(evaluationStatus == true) ? (
+          <>
+            <h1 className="text-2xl sm:text-4xl lg:text-6xl mt-24 font-semibold">
+              <span className="gradient-text">Training</span> Modules.
+            </h1>
+            <h1 className="text-md sm:text-2xl lg:text-3xl opacity-70">
+              Our Recommendations
+            </h1>
+            <div className="flex gap-4 flex-wrap text-light text-xl font-medium mt-8">
+              {
+                category == "Younger" && (months / 12) <= 6 ? (
+                  <>
+                    <Link className="card bg-primary w-96 grow hover:grow-[2] grid text-center py-16" to={"/training/3dtext"}>
+                      3D Alphabets
+                    </Link>
+                  </>) : (<>
+                    <Link className="card bg-primary w-96 grow hover:grow-[2] grid text-center py-16" to={"/training/colorMatch"}>
+                      Color Matching
+                    </Link>
+                    <Link className="card bg-primary w-96 grow hover:grow-[2] grid text-center py-16" to={"/training/shapeDetection"}>
+                      Shape Detection
+                    </Link>
+                  </>)
+              }
+              {((months / 12) >= 6 && physicalResult < 7842 ? (<>
+                <Link className="card bg-primary w-96 grow hover:grow-[2] grid text-center py-16" to={"/training/colorMatch"}>
+                  Color Matching
+                </Link>
+                <Link className="card bg-primary w-96 grow hover:grow-[2] grid text-center py-16" to={"/training/shapeDetection"}>
+                  Shape Detection
+                </Link>
+                <Link className="card bg-primary w-96 grow hover:grow-[2] grid text-center py-16" to={"/training/detection"}>
+                  Object Recognition
+                </Link>
+              </>) : (null))}
+              {((months / 12) >= 6 && physicalResult >= 7842) ? (<>
+                <Link className="card bg-primary w-96 grow hover:grow-[2] grid text-center py-16" to={"/training/faceExpressionDetection"}>
+                  Face Expression Detection
+                </Link>
+                <Link className="card bg-primary w-96 grow hover:grow-[2] grid text-center py-16" to={"/training/memoryGame"}>
+                  Memory Game
+                </Link>
+              </>) : (null)}
+            </div>
+          </>
+        ) : null}
+
       </section>
     </>
   )
